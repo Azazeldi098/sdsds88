6eabc8f57e7b2a354fe94495a131fde51c4a313e
Azazeldi098 <Azazeldi88@gmail.com>
2026-06-10 12:51:13 +0500

    quick.py

--- quick.py
+++ quick.py
@@ -1,12 +1,29 @@
 def quick_sort(arr, key=lambda x: x):
-    if len(arr) <= 1:
-        return arr
+    a = list(arr)
     
-    pivot = arr[len(arr) // 2]
-    pivot_val = key(pivot)
-    
-    left = [x for x in arr if key(x) < pivot_val]
-    middle = [x for x in arr if key(x) == pivot_val]
-    right = [x for x in arr if key(x) > pivot_val]
-    
-    return quick_sort(left, key) + middle + quick_sort(right, key)
+    def _quick_sort(left_idx, right_idx):
+        if left_idx >= right_idx:
+            return
+        
+        pivot = a[(left_idx + right_idx) // 2]
+        pivot_val = key(pivot)
+        
+        i = left_idx
+        j = right_idx
+        
+        while i <= j:
+            while key(a[i]) < pivot_val:
+                i += 1
+            while key(a[j]) > pivot_val:
+                j -= 1
+                
+            if i <= j:
+                a[i], a[j] = a[j], a[i]
+                i += 1
+                j -= 1
+        
+        _quick_sort(left_idx, j)
+        _quick_sort(i, right_idx)
+
+    _quick_sort(0, len(a) - 1)
+    return a
